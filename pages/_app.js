d8b39652cda109eaa09b09aa991ea2da3a63daf3
githubotoro <yupuday.git@gmail.com>
2022-12-23 15:39:17 +0530

    google analytics update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { useRouter } from "next/router";
+import { PageView } from "../lib/ga";
+import { useEffect } from "react";
 
 const App = ({ Component, pageProps }) => {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			pageview(url);
+		};
+
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
 			<div className="font-Roboto">
