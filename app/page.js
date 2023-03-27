c254a118e82ee112e35975e9d32dd3714c5b4830
githubotoro <yupuday.git@gmail.com>
2023-03-27 23:20:50 +0530

    home page updated finally

--- app/page.js
+++ app/page.js
@@ -1,13 +1,56 @@
+import Link from "next/link";
+
 const Home = () => {
-	return (
-		<main className="flex flex-col items-center w-full h-full place-content-center">
-			{/* <div
-				className="flex flex-col items-center px-3 py-0 text-lg font-medium transition-all duration-300 ease-in-out rounded-lg shadow-sm outline-none cursor-pointer place-content-center text-isLabelLightPrimary bg-isSystemLightSecondary hover:bg-isBlueDark hover:text-isSystemLightSecondary hover:rounded-xl hover:text-xl focus:outline-none shadow-isFillDarkPrimary"
-			>
-				Button
-			</div> */}
-		</main>
-	);
+  return (
+    <>
+      <div className="space absolute w-full h-full opacity-50 -z-10"></div>
+      <main className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center place-content-center w-full h-[500px] sm:h-[600px]">
+          <div className="grid grid-cols-1 w-full gap-12 sm:gap-16 items-center">
+            <h1 className="text-3xl sm:text-6xl sm:leading-[3.7rem] text-center font-800 px-3">
+              <span className="text-transparent drop-shadow-xl bg-clip-text bg-gradient-to-b from-isWhite to-isSystemLightTertiary">
+                build cool products
+              </span>
+              <br />
+              <span className="text-transparent bg-clip-text bg-gradient-to-b from-isGreenDark to-isGreenLightEmphasis">
+                in minutes
+              </span>
+              &nbsp;
+              <span className="text-transparent bg-clip-text bg-gradient-to-b from-isSystemDarkTertiary to-isSystemDarkSecondary">
+                not hours
+              </span>
+            </h1>
+
+            <div className="flex flex-col items-center">
+              <p className="max-w-xs sm:max-w-3xl drop-shadow-sm text-isSystemLightTertiary/25 text-center text-xl leading-6 sm:text-3xl w-full  px-3 font-700">
+                we create <span className="text-isYellowDark">notes</span>
+                &nbsp;from web3 <span className="text-isRedDark">docs</span>, so
+                you can start&nbsp;
+                <span className="text-isBlueDark">shippin'</span> your dream
+                project{" "}
+                <span className="text-isSystemLightSecondary">
+                  @ lightspeed
+                </span>
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <Link href={`/sea`}>
+                <button
+                  className="w-fit bg-isSystemLightSecondary font-800 tracking-wide text-isSystemDarkTertiary
+			  px-[0.6rem] sm:px-[0.8rem] sm:py-[0.1rem]
+		rounded-lg sm:rounded-xl text-2xl sm:text-3xl shadow-sm transition-all hover:bg-isWhite hover:text-isSystemDarkSecondary
+		duration-300 ease-in-out hover:drop-shadow-[0_0_15px_rgba(255,255,255,0.3)]"
+                >
+                  let's ship
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
